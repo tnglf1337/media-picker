817a48a10e5bb159ef79c65ffd8779990b1a1a6d
Timo Neske <neske.timo93@gmail.com>
2025-10-13 21:27:22 +0200

    add method

--- backend/src/main/java/com/mediapicker/service/MediathekService.java
+++ backend/src/main/java/com/mediapicker/service/MediathekService.java
@@ -1,19 +1,39 @@
 package com.mediapicker.service;
 
 import com.mediapicker.db.MediathekDao;
+import com.mediapicker.db.UserDao;
 import com.mediapicker.domain.mediathek.Mediathek;
+import com.mediapicker.domain.mediathek.medium.Medium;
+import com.mediapicker.domain.user.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 public class MediathekService {
 
-  private final MediathekDao mediathekDao;
+  @Value("${app.offline.default.username}")
+  private String username;
 
-  public MediathekService(MediathekDao mediathekDao) {
+  private final MediathekDao mediathekDao;
+  private final UserDao userDao;
+
+  public MediathekService(MediathekDao mediathekDao, UserDao userDao) {
     this.mediathekDao = mediathekDao;
+    this.userDao = userDao;
   }
 
   public void save(Mediathek mediathek) {
+    mediathekDao.save(mediathek);
+  }
+
+  @Transactional
+  public void mediumErstellen(Medium medium) {
+    User user = userDao.findByUsername(username);
+    Mediathek mediathek = mediathekDao.findByUser(user);
+    mediathek.mediumHinzufügen(medium);
     mediathekDao.save(mediathek);
   }
 }
