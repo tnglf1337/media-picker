1d398d88195dda0e0e7ba294d04619329f1f73d0
Timo Neske <neske.timo93@gmail.com>
2025-10-13 21:26:02 +0200

    add method

--- backend/src/main/java/com/mediapicker/db/MediathekDao.java
+++ backend/src/main/java/com/mediapicker/db/MediathekDao.java
@@ -1,11 +1,14 @@
 package com.mediapicker.db;
 
 import com.mediapicker.domain.mediathek.Mediathek;
+import com.mediapicker.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
 public interface MediathekDao extends JpaRepository<Mediathek, UUID> {
+
+  Mediathek findByUser(User user);
 
 }
