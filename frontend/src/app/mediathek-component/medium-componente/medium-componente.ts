34a37c1266317012d579d127d3ef6bf937c82cfa
Timo Neske <neske.timo93@gmail.com>
2025-10-14 23:17:27 +0200

    update component

--- frontend/src/app/mediathek-component/medium-componente/medium-componente.ts
+++ frontend/src/app/mediathek-component/medium-componente/medium-componente.ts
@@ -1,13 +1,14 @@
-import {AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit, signal, ViewChild} from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {Medium, MediumTyp} from '../../domain/medium';
+import {Buch, Film, Manga, Medium, MediumTyp, Podcast, Serie, Videospiel} from '../../domain/medium';
 import {MediumErstellenForm} from './medium-erstellen-form/medium-erstellen-form';
 import {MediumApiService} from '../../service/api.service';
+import {NgForOf} from '@angular/common';
 
 
 @Component({
   selector: 'app-medium-componente',
-  imports: [MediumErstellenForm],
+  imports: [MediumErstellenForm, NgForOf],
   templateUrl: './medium-componente.html',
   styleUrl: './medium-componente.css'
 })
@@ -26,10 +27,20 @@ export class MediumComponente implements OnInit{
     this.route.params.subscribe(params => {
       this.mediumTyp = params['mediumTyp'];
       this.mediumAuswahl = this.mediumHeader
-      this.mediumApiService.getMedienByMediumTyp(this.mediumTyp).subscribe(medien => {
-        this.medien = medien;
-        console.log("got medien: ", this.medien)
-      })
+      this.mediumApiService.getMedienByMediumTyp(this.mediumTyp).subscribe(arr => {
+        this.medien = arr.map(m => {
+          switch (this.mediumTyp) {
+            case MediumTyp.SERIE: return Object.assign(new Serie(), m);
+            case MediumTyp.ANIME: return Object.assign(new Serie(), m);
+            case MediumTyp.MANGA: return Object.assign(new Manga(), m);
+            case MediumTyp.FILM: return Object.assign(new Film(), m);
+            case MediumTyp.VIDEOSPIEL: return Object.assign(new Videospiel(), m);
+            case MediumTyp.BUCH: return Object.assign(new Buch(), m);
+            case MediumTyp.PODCAST: return Object.assign(new Podcast(), m);
+            default: return Object.assign(new Medium(), m);
+          }
+        });
+      });
     });
   }
 
