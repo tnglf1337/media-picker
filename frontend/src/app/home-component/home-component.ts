7168c402c5b97a2669c247451fccec3072305cf2
Timo Neske <neske.timo93@gmail.com>
2025-10-23 20:45:46 +0200

    Implement method wuerfelCurrentMedien

--- frontend/src/app/home-component/home-component.ts
+++ frontend/src/app/home-component/home-component.ts
@@ -1,6 +1,7 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {MediumApiService} from '../service/api.service';
 import { CommonModule } from '@angular/common';
+import {RandomMediumService} from './random-medium.service';
 
 @Component({
   selector: 'app-home-component',
@@ -10,14 +11,21 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeComponent implements OnInit {
   apiService : MediumApiService = inject(MediumApiService)
+  randomMediumService! : RandomMediumService;
   currentMedien : { [key: string]: any[] } = {}
+  randomMedium : any = null;
 
   ngOnInit(): void {
     this.apiService.getCurrentMedien().subscribe({
       next: data => {
         this.currentMedien = data;
         console.log(this.currentMedien);
+        this.randomMediumService = new RandomMediumService(this.currentMedien)
       }
     })
+  }
+
+  wuerfelCurrentMedien() {
+    this.randomMedium = this.randomMediumService.getRandomMedium();
   }
 }
