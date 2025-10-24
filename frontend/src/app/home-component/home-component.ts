1a24c3e45ef2ced3840f1a331fe5a697801c3817
Timo Neske <neske.timo93@gmail.com>
2025-10-24 20:36:01 +0200

    Update html structure

--- frontend/src/app/home-component/home-component.ts
+++ frontend/src/app/home-component/home-component.ts
@@ -2,10 +2,11 @@ import {Component, inject, OnInit} from '@angular/core';
 import {MediumApiService} from '../service/api.service';
 import { CommonModule } from '@angular/common';
 import {RandomMediumService} from './random-medium.service';
+import {StatistikComponent} from './statistik-component/statistik-component';
 
 @Component({
   selector: 'app-home-component',
-  imports: [CommonModule],
+  imports: [CommonModule, StatistikComponent],
   templateUrl: './home-component.html',
   styleUrls: ['./home-component.css', '../../styles.css']
 })
