fdaab0561bb24247b7e8e0e1e383e7dc135c3243
Timo Neske <Timo.Neske@hhu.de>
2025-11-22 20:56:24 +0100

    Update files

--- backend/src/main/java/com/mediapicker/web/MediathekController.java
+++ backend/src/main/java/com/mediapicker/web/MediathekController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:4201"})
+@CrossOrigin(origins = "*")
 @RestController
 public class MediathekController {
 
--- backend/src/main/java/com/mediapicker/web/MediumErstellenController.java
+++ backend/src/main/java/com/mediapicker/web/MediumErstellenController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/medium-erstellen")
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:4201"})
+@CrossOrigin(origins = "*")
 public class MediumErstellenController {
 
   private final MediathekService mediathekService;
